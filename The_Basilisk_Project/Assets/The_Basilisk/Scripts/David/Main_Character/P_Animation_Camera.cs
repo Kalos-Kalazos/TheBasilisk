f46927516604520c6a4e62b296139ea46422b0d0
fallen30082002 <bresosanchezdavid@gmail.com>
2024-12-03 18:54:47 +0100

    First Animation Camera

--- The_Basilisk_Project/Assets/The_Basilisk/Scripts/David/Main_Character/P_Animation_Camera.cs
+++ The_Basilisk_Project/Assets/The_Basilisk/Scripts/David/Main_Character/P_Animation_Camera.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class P_Animation_Camera : MonoBehaviour
 {
-    
+    public Animator animation;
+    private P_Character_Move playerMoving;
+
+    private bool isWalkingLastState = false;
+
+    private void Start()
+    {
+        if (!animation)
+        {
+            animation = GetComponent<Animator>();
+            if (!animation)
+            {
+                Debug.LogError("Animator no encontrado. Por favor, asigna un Animator desde el Inspector.");
+            }
+        }
+
+        playerMoving = FindObjectOfType<P_Character_Move>();
+        if (!playerMoving)
+        {
+            Debug.LogError("No se encontró un objeto de tipo P_Character_Move en la escena.");
+        }
+    }
+
+    private void Update()
+    {
+        if (playerMoving != null && animation != null)
+        {
+            bool isWalking = playerMoving.isWalking;
+            if (isWalking != isWalkingLastState) 
+            {
+                animation.SetBool("isWalking", isWalking);
+                isWalkingLastState = isWalking;
+            }
+        }
+    }
 }
