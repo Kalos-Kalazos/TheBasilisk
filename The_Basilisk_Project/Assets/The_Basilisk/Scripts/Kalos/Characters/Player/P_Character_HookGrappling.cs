43da007e3a93d207106c72e13e09fb85bb61d2ac
Kalos Kalazos <carlos.csm44@gmail.com>
2024-12-04 16:26:55 +0100

    freeze when hook

--- The_Basilisk_Project/Assets/The_Basilisk/Scripts/Kalos/Characters/Player/P_Character_HookGrappling.cs
+++ The_Basilisk_Project/Assets/The_Basilisk/Scripts/Kalos/Characters/Player/P_Character_HookGrappling.cs
@@ -7,6 +7,7 @@ public class P_Character_HookGrappling : MonoBehaviour
 {
     [Header("=== References ===")]
     [SerializeField] P_Character_Move pm;
+    [SerializeField] Rigidbody rb;
     [SerializeField] Transform cam;
     [SerializeField] Transform gunTip;
     [SerializeField] LayerMask isGrappleable;
@@ -20,11 +21,14 @@ public class P_Character_HookGrappling : MonoBehaviour
 
     private Vector3 grapplePoint;
 
-    private bool grappling;
+    //Grapple bools
+    private bool grappling, freeze, activeGrapple;
 
     void Start()
     {
         pm = GetComponent<P_Character_Move>();
+
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -38,6 +42,13 @@ public class P_Character_HookGrappling : MonoBehaviour
         if (grapplingCDTimer < 0) grapplingCDTimer = 0;
         #endregion
 
+        if (freeze)
+        {
+            pm.playerSpeed = 0;
+            rb.velocity = Vector3.zero;
+        }
+        else
+            pm.playerSpeed = 10;
     }
 
     private void LateUpdate()
@@ -59,11 +70,14 @@ public class P_Character_HookGrappling : MonoBehaviour
         StopGrapple();
     }*/
 
+
     private void StartGrapple()
     {
         if (grapplingCDTimer > 0) return;
 
         grappling = true;
+
+        freeze = true;
 
         RaycastHit hit;
         if(Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance, isGrappleable))
@@ -85,15 +99,37 @@ public class P_Character_HookGrappling : MonoBehaviour
 
     private void ExecuteGrapple()
     {
-
+        freeze = false;
     }
 
     private void StopGrapple()
     {
+        freeze = false;
+
         grappling = false;
 
         grapplingCDTimer = grapplingCD;
 
         lr.enabled = false;
+    }
+
+    public void JumpToPosition(Vector3 targetPosition, float trajectoryHeight)
+    {
+        activeGrapple = true;
+
+        rb.velocity = CalculateJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+    }
+
+    public Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravity = Physics.gravity.y;
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.y, 0f, endPoint.z - startPoint.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = displacementXZ * (Mathf.Sqrt(-2 * trajectoryHeight / gravity)
+        +Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+
+        return velocityXZ + velocityY;
     }
 }
