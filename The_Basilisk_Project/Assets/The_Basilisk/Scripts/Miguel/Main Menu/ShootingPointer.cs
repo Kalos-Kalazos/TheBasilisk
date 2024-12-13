7e5be480351fba24398f8ab50cbc4ac02cdb3c1c
MikeChase <comandantechase.99@gmail.com>
2024-12-13 18:52:13 +0100

    Point_UI

---
+++ The_Basilisk_Project/Assets/The_Basilisk/Scripts/Miguel/Main Menu/ShootingPointer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingPointer : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (target != null)
+        {
+
+            transform.LookAt(target);
+
+        }
+
+    }
+}
