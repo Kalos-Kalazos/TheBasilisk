c412b53fa3aa46d160b6b32c538e1728e41197c5
MikeChase <comandantechase.99@gmail.com>
2025-01-22 20:13:25 +0100

    Main Menu: UI

---
+++ The_Basilisk_Project/Assets/The_Basilisk/Scripts/Miguel/Main Menu/Pause/Buttons_Pause/ButtonsPause.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonsPause : MonoBehaviour
+{
+
+    public GameObject PauseMenuUI;//Referencia al panel del de pausa
+    public GameObject optionsMenuUI; //Referencia al panel de opciones
+    public GameObject mainMenuUI; //Referencia al panel del menu principal
+
+    private bool isPaused = false;
+
+    public void Resume()
+    {
+
+        PauseMenuUI.SetActive(false); //Oculta el menu de pausa
+        Time.timeScale = 1f; //Reanuda el tiempo
+        isPaused = false;
+
+    }
+
+    public void OpenOptions()
+    {
+
+        PauseMenuUI.SetActive(false); //Oculta el menu de pausa
+        optionsMenuUI.SetActive(true); //Oculta el menu de opciones
+
+    }
+
+    public void BackToPauseMenu()
+    {
+
+        optionsMenuUI.SetActive(false); //Oculta el menu de opciones
+        PauseMenuUI.SetActive(true); // vuelve al menu de pausa
+
+    }
+
+    public void LoadMainMenu()
+    {
+
+        Time.timeScale = 1f;// Asegurar de que reanuda el tiempo
+
+        mainMenuUI.SetActive(true); //Muestra el Menu principal
+        //alternativamente carga la scena del menu principal:
+
+    }
+
+    public void QuitGame()
+    {
+
+        Debug.Log("Saliendo del Juego...");//funciona al copilar el juego
+
+    }
+
+}
