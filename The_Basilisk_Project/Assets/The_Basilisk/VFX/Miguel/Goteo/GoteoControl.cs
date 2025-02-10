ad7fd96cba82f38f6eb5e6abfef4ca6a4fe14330
MikeChase <comandantechase.99@gmail.com>
2025-02-10 18:17:56 +0100

    VFX: Configuraciones

--- The_Basilisk_Project/Assets/The_Basilisk/VFX/Miguel/Goteo/GoteoControl.cs
+++ The_Basilisk_Project/Assets/The_Basilisk/VFX/Miguel/Goteo/GoteoControl.cs
@@ -6,14 +6,16 @@ public class GoteoControl : MonoBehaviour
 {
     public ParticleSystem goteo;
     public float intervalo = 1f;
-
+    public float offsetMax = 2f; // Máximo retraso aleatorio al inicio
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // Generamos un retraso aleatorio antes de comenzar la emisión
+        float offset = Random.Range(0, offsetMax);
         InvokeRepeating("EmitirGota", 0, intervalo);
-
+        
+        
     }
 
     void EmitirGota()
@@ -21,7 +23,7 @@ public class GoteoControl : MonoBehaviour
 
         if (goteo !=null)
         {
-            goteo.Emit(Random.Range(1, 2)); //Emite entre 1 y 2 gotas
+           goteo.Emit(Random.Range(1, 2)); //Emite entre 1 y 2 gotas
         }
 
     }
