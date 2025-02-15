61bb3e229cd3b6c12e097306ede2d788e2666d2b
MikeChase <comandantechase.99@gmail.com>
2025-02-15 17:33:46 +0100

    Heal effect

--- The_Basilisk_Project/Assets/The_Basilisk/Scripts/Miguel/Health/PickMedikit/Medikit.cs
+++ The_Basilisk_Project/Assets/The_Basilisk/Scripts/Miguel/Health/PickMedikit/Medikit.cs
@@ -7,7 +7,7 @@ public class Medikit : MonoBehaviour
 {
 
     public BatterySystem batterySystem; // Referencia al batterySystem
-
+    public ParticleSystem pickupEffect; // Referencia al sistema de particulas (efecto)
 
     // Este Metodo se llama cuando otro objeto entra en el trigger
 
@@ -16,9 +16,22 @@ public class Medikit : MonoBehaviour
         if(other.CompareTag("Player")) //Asegurar que es el jugador el que choca
         {
 
-            batterySystem.AddBattery(); //sumar una pila al sistema
+            // Verifica si el sistema de batería está asignado
+            if (batterySystem != null)
+            {
 
-            Destroy(gameObject); // Elimina el medikit del juego despues de recogerlo
+                batterySystem.AddBattery(); //sumar una pila al sistema
+               
+                //Activar el efecto de particulas
+
+                if (pickupEffect != null)
+                {
+
+                    Instantiate(pickupEffect, transform.position, Quaternion.identity); //Instanciar el efecto en la posición del medikit
+
+                }
+                Destroy(gameObject); // Elimina el medikit del juego despues de recogerlo
+            }
         }
 
     }
